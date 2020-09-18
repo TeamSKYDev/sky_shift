34773deb11b4fcea4cd0adbc3bb023d91b36b230
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-09-18 19:33:30 +0900

    finish login_system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # deviseのルートの上に他ルートを書かないこと。競合発生する可能性あり
+
+  get "home" => "homes#home", as: "home"
+  root "homes#top"
+
+
 end
