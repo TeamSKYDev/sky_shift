88379ebcd44c6507de10092fcf4f9a2a09d37148
sato9818 <kouki9818@gmail.com>
2020-10-01 17:44:33 +0900

    finish private calendar

--- app/controllers/private_schedules_controller.rb
+++ app/controllers/private_schedules_controller.rb
@@ -2,10 +2,34 @@ class PrivateSchedulesController < ApplicationController
 
     def new
         @private_schedule = PrivateSchedule.new
+        date = Date.parse(params[:date])
+        @private_schedule.start_time = (date + Time.parse("09:00").seconds_since_midnight.seconds).to_datetime
+        @private_schedule.end_time = (date + Time.parse("10:00").seconds_since_midnight.seconds).to_datetime
     end
 
     def edit
-        @private_schedule = PrivateSchedule.find(params[:id])
+        @private_schedule = current_user.private_schedules.find(params[:id])
+    end
+
+    def update
+        @private_schedule = current_user.private_schedules.find(params[:id])
+        respond_to do |format|
+            if @private_schedule.update(private_shcedule_params)
+                # format.html { redirect_to staffs_path, notice: 'User was successfully created.' }
+                # format.json { render :edit, status: :created, location: @staff }
+                format.js { @status = "success" }
+            else
+                # format.html { render :new }
+                # format.json { render json: @staff.errors, status: :unprocessable_entity }
+                format.js { @status = "fail" }
+            end
+        end
+    
+    end
+
+    def destroy
+        @private_schedule = current_user.private_schedules.find(params[:id])
+        @private_schedule.destroy
     end
 
     def create
--- app/models/private_schedule.rb
+++ app/models/private_schedule.rb
@@ -1,8 +1,18 @@
 class PrivateSchedule < ApplicationRecord
     belongs_to :user
 
+    validates :title, presence: true
+    validate :end_time_is_after_start_time
 
     def start_date
         return start_time.to_date
     end
+
+    private
+    def end_time_is_after_start_time
+  
+    if end_time < start_time
+      errors.add(:end_time, "cannot be before the start date") 
+    end 
+  end
 end
