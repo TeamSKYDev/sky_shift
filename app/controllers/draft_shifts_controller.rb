4c5aafeca4d2e9bf196deb974cb5c0276af20de6
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-10-09 16:27:47 +0900

    finish actions

--- app/controllers/draft_shifts_controller.rb
+++ app/controllers/draft_shifts_controller.rb
@@ -29,20 +29,46 @@ class DraftShiftsController < ApplicationController
 		else
 			@date = Date.current.beginning_of_month
 		end
-		@submitted_shifts = SubmittedShift.where(store_id: @store.id, start_time: @date.in_time_zone.all_day, status: true)
-		@draft_shifts = DraftShift.where(store_id: @store.id, start_time: @date.in_time_zone.all_day)
+		@submitted_shifts = SubmittedShift.where(store_id: @store.id, start_time: @date.in_time_zone.all_day, status: true).order(:start_time)
+		@draft_shifts = DraftShift.where(store_id: @store.id, start_time: @date.in_time_zone.all_day).order(:start_time)
 		@draft_shift = DraftShift.new
-		user_ids = Staff.where(store_id: @store.id, is_permitted: true).pluck(:user_id)
+		@draft_shift.start_time = @date
+		@draft_shift.end_time = @date
+		user_ids = Staff.where(store_id: @store.id, is_permitted_status: true, is_unsubscribe: false).pluck(:user_id)
+		# @usersにヘルプを追加
 		@users = User.where(id: [user_ids])
 	end
 
 	def create
+		@draft_shift = DraftShift.new(draft_shift_params)
+		if @draft_shift.save!
+			flash[:notice] = "create new draft"
+		else
+			flash[:notice] = "cannot create draft"
+		end
+		redirect_to request.referer
 	end
 
 	def update
+		@draft_shift = DraftShift.find(params[:id])
+		if @draft_shift.update(draft_shift_params)
+			flash[:notice] = "update draft"
+		else
+			flash[:notice] = "cannot create draft"
+		end
+		redirect_to request.referer
 	end
 
 	def destroy
+		@draft_shift = DraftShift.find(params[:id])
+		@draft_shift.destroy
+		flash[:notice] = "destroy draft"
+		redirect_to daily_draft_shifts_path
+	end
+
+	private
+	def draft_shift_params
+		params.require(:draft_shift).permit(:store_id, :user_id, :start_time, :end_time)
 	end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+	email: "help@help",
+	password: "helphelp"
+	telephone_number: "help"
+	is_unsubscribe: true
+	last_name: "ヘルプ"
+)
