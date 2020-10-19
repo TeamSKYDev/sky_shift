9a0cd8316041372a4cc4f4c04cde6f647a775d7b
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2020-10-20 01:29:27 +0900

    made temporary calender

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -22,18 +22,37 @@ class HomesController < ApplicationController
 				@month_submitted_shifts = SubmittedShift.where(user_id: current_user.id, store_id: @store.id, start_time: Date.today.in_time_zone.all_month)
 			end
 		end
+		#ActiveRecord::Base.connection.drop_table('events')
+		ActiveRecord::Base.connection.create_table('events', temporary: true, force: true) do |t|
+		  # t.integer :user_id    , null: true
+		  t.string :title      , null: false
+		  t.text   :content
+		  t.datetime  :start_time , null: false
+		  t.datetime  :end_time
 
-		# @month_submitted_shifts.each do |submitted_shift|
-		# 	@event = Event.new
-		# 	if submitted_shift.status == true
-		# 		@event.title = submitted_shift.start_time.strftime("%H:%M") + "~" + submitted_shift.end_time.strftime("%H:%M")
-		# 	else
-		# 		@event.title = "未"+submitted_shift.start_time.strftime("%H:%M") + "~" + submitted_shift.end_time.strftime("%H:%M")
-		# 	end
-		# 	@event.start_time = submitted_shift.start_time
-		# 	@event.end_time = submitted_shift.end_time
-		# 	@event.save
-		# end
+		  t.timestamps null: false
+		end
+
+		klass = Class.new(ActiveRecord::Base) do |c|
+		  c.table_name = 'events'
+		end
+		Object.const_set('Event', klass)
+
+		if @month_submitted_shifts.present?
+			@month_submitted_shifts.each do |submitted_shift|
+				@event = Event.new
+				if submitted_shift.status == true
+					@event.title = submitted_shift.start_time.strftime("%H:%M") + "~" + submitted_shift.end_time.strftime("%H:%M")
+				else
+					@event.title = "未"+ submitted_shift.start_time.strftime("%H:%M") + "~" + submitted_shift.end_time.strftime("%H:%M")
+				end
+				@event.start_time = submitted_shift.start_time
+				@event.end_time = submitted_shift.end_time
+				@event.save!
+			end
+		end
+		@event = Event.new(start_time: Date.today, title: "aaa").save!
+		@events = Event.all
 
 		# @decided_shifts = DecidedShift.where(store_id: @store.id, user_id: current_user.id)
 	end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
-class Event < ApplicationRecord
-end
+
+
+	klass = Class.new(ActiveRecord::Base) do |c|
+	  c.table_name = 'events'
+	end
+	Object.const_set('Event', klass)
+
